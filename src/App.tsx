754366dbd6bdff71b3eb3875bc3f4971f087ae2d
Uandrik Laptop <uandriy4141@gmail.com>
2025-10-02 17:36:46 +1400

    Attempt1

--- src/App.tsx
+++ src/App.tsx
@@ -28,9 +28,17 @@ class App extends React.Component<{}, State> {
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          gap={10}
+          step={2}
+          frameSize={3}
+          itemWidth={130}
+          animationDuration={1000}
+          infinite={false}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+interface CaoruseImages {
+  images: string[];
+  gap: number;
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<CaoruseImages> = ({
+  images,
+  gap,
+  step,
+  itemWidth,
+  frameSize,
+  animationDuration,
+  infinite,
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const itemWidthWithGap = itemWidth + gap;
+  const transformValue = -currentIndex * itemWidthWithGap;
+
+  return (
+    <div className="Carousel">
+      <div className="Carousel__container">
+        <ul
+          style={{
+            width: `${itemWidthWithGap * frameSize - gap}px`,
+            transform: `translateX(${transformValue}px)`,
+            transition: `transform ${animationDuration}ms ease`,
+          }}
+          className="Carousel__list"
+        >
+          {images.map((i, index) => (
+            <li key={index} className="list__item">
+              <img
+                className="list__img"
+                style={{ width: `${itemWidth}`, height: `${itemWidth}` }}
+                src={i}
+                alt={`${index}`}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="buttons">
+        <button
+          data-cy="next"
+          onClick={() => {
+            setCurrentIndex(c => {
+              if (c === 0) {
+                return infinite ? images.length - step : 0;
+              }
+
+              return c - step;
+            });
+          }}
+          disabled={!infinite && currentIndex === 0}
+          className="button"
+          type="button"
+        >
+          &#x25C0;
+        </button>
+        <button
+          onClick={() => {
+            setCurrentIndex(c => {
+              if (c >= images.length - step) {
+                return infinite ? 0 : images.length - step;
+              }
+
+              return c + step;
+            });
+          }}
+          disabled={!infinite && currentIndex === images.length - step}
+          className="button"
+          type="button"
+        >
+          &#x25B6;
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
